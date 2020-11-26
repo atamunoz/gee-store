be3d2c636a801ea88ab8220d88a8845dec17f650
José María Martínez Luna <jmmluna@gmail.com>
2020-11-26 14:14:57 +0100

    Add files via upload

---
+++ gee_template-v1.0.0/gee_template-v1.0.0.js
@@ -0,0 +1,12 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"BCrq":[function(require,module,exports) {
+
+},{}],"wxzq":[function(require,module,exports) {
+module.exports="/gee-logo.a5f3b130.png";
+},{}],"fUdq":[function(require,module,exports) {
+"use strict";var e=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(exports,"__esModule",{value:!0}),exports.initialize=void 0,require("./ext.css");var t=e(require("./gee-logo.png"));exports.initialize=function(e,n){var i=document.createElement("div");i.className="card center",i.style.cssText="background-color:#ffffcc";var r=document.createElement("div");r.className="container",r.innerHTML="\n  <h4><b>GeoWE Extension Engine</b></h4>\n  <p>By GeoWE.org </p>";var o=document.createElement("img"),a=e[n].path;o.setAttribute("src",a+"/"+t.default),o.style.cssText="width:50%",i.appendChild(o),i.appendChild(r),document.body.appendChild(i),setTimeout(function(){i.remove()},5e3)};
+},{"./ext.css":"BCrq","./gee-logo.png":"wxzq"}],"epUE":[function(require,module,exports) {
+module.exports={id:"gee1606396361538"};
+},{}],"ZSQl":[function(require,module,exports) {
+var global = arguments[3];
+var e=arguments[3],i=require("../src/index"),r=u(require("./gee-id.json"));function u(e){return e&&e.__esModule?e:{default:e}}e[r.default.id]=i.initialize;
+},{"../src/index":"fUdq","./gee-id.json":"epUE"}]},{},["ZSQl"], null)
